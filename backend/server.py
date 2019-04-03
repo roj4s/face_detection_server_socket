f3168bcc852612a849ad9b899ec89450d4cb3c84
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2019-04-03 17:51:24 -0400

    Addapted to use face-detector

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_socketio import SocketIO
-from face_recognition_manager.face_alignment.mtcnn import FaceAlignmentMTCNN
+import face_detector as fd
 import cv2
 import numpy as np
+import tempfile
 
-face_aligner= FaceAlignmentMTCNN("/home/neo/dev/face_recognition/face_recognition_manager/data/models/mtcnn",
-                     config={'gpu_memory_fraction': 0.2})
+detector = fd.FaceDetector()
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -13,21 +13,27 @@ socketio = SocketIO(app)
 @socketio.on('detect')
 def handle_message(data):
     print("Got new image ...")
-    try:
-        img_data = np.fromstring(data['data'], dtype=np.uint8)
-        img = cv2.imdecode(img_data, 1)
-        '''
-        # Uncomment to see frontend sent image
-        cv2.imshow('img', img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        '''
-        bb = face_aligner.find_one_face(img)
+    #try:
+    img_data = np.fromstring(data['data'], dtype=np.uint8)
+    img = cv2.imdecode(img_data, 1)
+    temp_file = tempfile.NamedTemporaryFile()
+    img_path = "{}.jpg".format(temp_file.name)
+    cv2.imwrite(img_path, img)
+    '''
+    # Uncomment to see frontend sent image
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    '''
+    faces = detector.get_faces(img_path)
+    for face in faces:
+        bb = face.bounding_box
+        socketio.emit('detected', {'x': bb.x, 'y': bb.y, 'w':
+                               bb.w, 'h': bb.h})
+    temp_file.close()
 
-        socketio.emit('detected', {'x': bb.x_float, 'y': bb.y_float, 'w':
-                                   bb.w_float, 'h': bb.h_float});
-    except Exception as e:
-        print("Error: {}".format(str(e)))
+    #except Exception as e:
+    #    print("Error: {}".format(str(e)))
 
 if __name__ == '__main__':
     print("Starting server ...")
