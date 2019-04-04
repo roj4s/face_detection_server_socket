6df6e773f41f3642695a86583351243a1fe4cef3
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2019-04-04 08:33:13 -0400

    Added backend requirements.txt

--- backend/server.py
+++ backend/server.py
@@ -12,23 +12,23 @@ socketio = SocketIO(app)
 @socketio.on('detect')
 def handle_message(data):
     print("Got new image ...")
-    #try:
-    img_data = np.fromstring(data['data'], dtype=np.uint8)
-    img = cv2.imdecode(img_data, 1)
-    '''
-    # Uncomment to see frontend sent image
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    '''
-    faces = detector.get_faces_from_img(img)
-    for face in faces:
-        bb = face.bounding_box
-        socketio.emit('detected', {'x': bb.x, 'y': bb.y, 'w':
-                               bb.w, 'h': bb.h})
+    try:
+        img_data = np.fromstring(data['data'], dtype=np.uint8)
+        img = cv2.imdecode(img_data, 1)
+        '''
+        # Uncomment to see frontend sent image
+        cv2.imshow('img', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        '''
+        faces = detector.get_faces_from_img(img)
+        for face in faces:
+            bb = face.bounding_box
+            socketio.emit('detected', {'x': bb.x, 'y': bb.y, 'w':
+                                   bb.w, 'h': bb.h})
 
-    #except Exception as e:
-    #    print("Error: {}".format(str(e)))
+    except Exception as e:
+        print("Error: {}".format(str(e)))
 
 if __name__ == '__main__':
     print("Starting server ...")
